d12bc65433a658b4ca45c98d6e56e08546585a1c
Kirill <134396570+QA-Kir@users.noreply.github.com>
2023-07-31 15:07:21 +0300

    Update test_01.py

--- Playwright/tests/test_01.py
+++ Playwright/tests/test_01.py
@@ -17,22 +17,21 @@ def test_post_user():
     r = requests.post(url=url_create_user,data=params_create_user)
     assert r.status_code == 201    
 
-#Это для ДЗ
-#1 case
+#HW 1 case
 def test_user_not_found():
     r = requests.get(url_single_user_not_found)
     assert r.status_code == 404
     req = r.json()
     assert(req) == {}
 
-#2 case
+#HW 2 case
 def test_list_resource():
     r = requests.get(url_list_resource)
     assert r.status_code == 200
     req = r.json()
     assert (req['data'][2]['name']) == 'true red'
 
-#3 case
+#HW 3 case
 def test_single_resource_not_found():
     r = requests.get(url_single_resource_404)
     assert r.status_code == 404
